d6505ea70bf956e4ba6eb82fb8662f3657cbf369
araqelyan398555 <art.araqelyan@hotmail.com>
2018-02-20 16:44:09 +0400

    make it better

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:'mongodb://<dbuser>:<dbpassword>@ds143778.mlab.com:43778/stroybooks-dev',
+    googleClientID: '116982091732-lvddiq16hfug0ekt6cjtchju4tss4p6n.apps.googleusercontent.com',
+    googleClientSecret: 'o2y_sNuC3vPdACEXGD7ZYxOA'
+}
